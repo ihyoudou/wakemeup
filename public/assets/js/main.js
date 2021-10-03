8ed38cf626f1d510b16f0a2c1ae840125549863c
ihyoudou <issei@issei.space>
2021-10-03 19:31:41 +0200

    frontpage in making

---
+++ public/assets/js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    document.getElementById("addButton").addEventListener("click", function() {
+        let hcaptchaResult = document.getElementsByName("g-recaptcha-response")[0].value;
+        let url = document.getElementById('url').value;
+        document.getElementById("addButton").disabled = true; 
+        (async () => {
+            const rawResponse = await fetch('/api/v1/addURL', {
+                method: 'POST',
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({url: url, captchaResponse: hcaptchaResult})
+            });
+            const content = await rawResponse.json();
+
+            console.log(content);
+            
+            if(content['success'] == true){
+                alert("Your URL was added!");
+            } else {
+                if(content['reason'] == "urlExist"){
+                    alert("This URL is already in database!");
+                } else if(content['reason'] == "missingBodyOrBadURL"){
+                    alert("The URL your entered is invalid");
+                } else {
+                    alert("Unknown error, please try again later");
+                }
+            }
+            document.getElementById("addButton").disabled = false; 
+        })();
+        
+    }); 
+});
--- public/index.php
+++ public/index.php
@@ -1,89 +1,125 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
-use Slim\Http\Response as Response;
-use Slim\Factory\AppFactory;
-use Slim\Views\Twig;
-use Slim\Views\TwigMiddleware;
+    require __DIR__ . '/../vendor/autoload.php';
+    use Slim\Http\Response as Response;
+    use Slim\Factory\AppFactory;
+    use Slim\Views\Twig;
+    use Slim\Views\TwigMiddleware;
 
-// loading env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
-$dotenv->safeLoad();
+    // loading env file
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+    $dotenv->safeLoad();
 
-$db = new MongoDB\Client($_ENV['MONGODB']);
-$collection = $db->wakemeup->towakeup;
+    $db = new MongoDB\Client($_ENV['MONGODB']);
+    $collection = $db->wakemeup->towakeup;
 
-// Create App
-$app = AppFactory::create();
+    // Create App
+    $app = AppFactory::create();
 
-// Create Twig
-$twig = Twig::create('../templates/', ['cache' => '../cache/']);
+    // Create Twig
+    $twig = Twig::create('../templates/', ['cache' => '../cache/']);
 
-// Add Twig-View Middleware
-$app->add(TwigMiddleware::create($app, $twig));
+    // Add Twig-View Middleware
+    $app->add(TwigMiddleware::create($app, $twig));
 
-// Define named route
-$app->get('/', function ($request, $response, $args) {
-    $view = Twig::fromRequest($request);
-    return $view->render($response, 'index.html', [
-        'appname' => $_ENV['APP_NAME']
-    ]);
-})->setName('index');
+    // Define named route
+    $app->get('/', function ($request, $response, $args) {
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'index.html', [
+            'hcaptchaSiteKey'=>$_ENV['hcaptchaSiteKey'],
+            'appname' => $_ENV['APP_NAME']
+        ]);
+    })->setName('index');
 
-$app->post('/add', function ($request, $response, $args) {
-    global $collection;
+    // /api/v1/getURLcount GET endpoint
+    // this endpoint is used to get current documents count in db
+    $app->get('/api/v1/getURLcount', function ($request, $response, $args) {
+        global $collection;
+        $count = $collection->count();
+        $jsonData = array(
+            'count'=>$count
+        );
+        return $response->withJson($jsonData, 200);
+    });
 
-    // retriving body and parsing json into array
-    $json = $request->getBody();
-    $data = json_decode($json, true);
-    
-    $url = $data['url'];
+    // /api/v1/addURL POST endpoint
+    // this endpoint is used to add a new link to system
+    $app->post('/api/v1/addURL', function ($request, $response, $args) {
+        global $collection;
 
-    // checking if array is not empty
-    if(!empty($data['url'])){
+        // retriving body and parsing json into array
+        $json = $request->getBody();
+        $data = json_decode($json, true);
         
-        $checkIfExist = $collection->find( [ 'url' => $url] )->toArray();
-        // checking if url is not already in db
-        if(!empty($checkIfExist[0]['url'])){
-            // if it is, returning 200 http code with success = false
-            $data = array(
-                'success'=> false,
-                'reason'=>'urlExist'
-            );
-            return $response->withJson($data, 200);
+        $url = $data['url'];
+
+
+        // checking if array is not empty and value is a valid url
+        if(!empty($data['url']) && filter_var($url, FILTER_VALIDATE_URL)){
+            
+            $checkIfExist = $collection->find( [ 'url' => $url] )->toArray();
+            // checking if url is not already in db
+            if(!empty($checkIfExist[0]['url'])){
+                // if it is, returning 200 http code with success = false
+                $data = array(
+                    'success'=> false,
+                    'reason'=>'urlExist'
+                );
+                return $response->withJson($data, 200);
+
+            } else {
+                // if it not exist - inserting into db and returning result
+                $addNewURL = $collection->insertOne(
+                    [
+                        'url'=>$url,
+                        'date'=>microtime(true)
+                    ]
+                );
+                // creating array with json response
+                $data = array(
+                    'success'=> true,
+                    'id'=>$addNewURL->getInsertedId()
+                );
+
+                return $response->withJson($data, 200);
+
+            }
+
+            
         } else {
-
-            // if it not exist - inserting into db and returning result
-            $addNewURL = $collection->insertOne(
-                [
-                    'url'=>$url,
-                    'date'=>microtime(true)
-                ]
-            );
-            // creating array with json response
+            // if body is empty
             $data = array(
-                'success'=> true,
-                'id'=>$addNewURL->getInsertedId()
+                    'success'=> false,
+                    'reason'=>'missingBodyOrBadURL'
             );
-
-            return $response->withJson($data, 200);
-
+            return $response->withJson($data, 500);
+                
         }
 
-        
-    } else {
-        // if body is empty
+    })->setName('addNew');
+
+    function validateCaptcha($response){
         $data = array(
-                'success'=> false,
-                'reason'=>'missingBody'
+            'secret' => $_ENV['hcaptchaSecret'],
+            'response' => $response
         );
-        return $response->withJson($data, 500);
-            
+        $verify = curl_init();
+        curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
+        curl_setopt($verify, CURLOPT_POST, true);
+        curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($verify);
+        // var_dump($response);
+        $responseData = json_decode($response);
+        if($responseData->success) {
+            // your success code goes here
+            return true;
+        } 
+        else {
+            // return error to user; they did not pass
+            return false;
+        }
     }
 
-    
 
-
-
-})->setName('addNew');
-// Run app
-$app->run();
+    // Run app
+    $app->run();
