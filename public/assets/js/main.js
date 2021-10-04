a20b5bf3dfaac11a1e87c3318bcb6a971053aec0
ihyoudou <issei@issei.space>
2021-10-04 13:12:00 +0200

    added bootstrap alerts instead of using js alerts

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         document.getElementById("pingcount").innerHTML = '<p class="text-muted fw-light">pinging '+count['count']+' urls</p>';
       });
     document.getElementById("addButton").addEventListener("click", function() {
+        document.getElementById('spinner').innerHTML = '<div class="spinner-border" role="status"></div>';
         let hcaptchaResult = document.getElementsByName("g-recaptcha-response")[0].value;
         let url = document.getElementById('url').value;
         document.getElementById("addButton").disabled = true; 
@@ -20,18 +21,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
             console.log(content);
             
             if(content['success'] == true){
-                alert("Your URL was added!");
+                document.getElementById("alerts").innerHTML = '<div class="alert alert-success" role="alert">Your URL was added!</div>';
+                document.getElementById("url").value = '';
+                // alert("Your URL was added!");
             } else {
+                hcaptcha.reset();
                 if(content['reason'] == "urlExist"){
-                    alert("This URL is already in database!");
+                    document.getElementById("alerts").innerHTML = '<div class="alert alert-warning" role="alert">This URL is already in database!</div>';
+                    // alert("This URL is already in database!");
                 } else if(content['reason'] == "missingBodyOrBadURL"){
-                    alert("The URL your entered is invalid");
+                    document.getElementById("alerts").innerHTML = '<div class="alert alert-warning" role="alert">The URL your entered is invalid</div>';
+                    // alert("The URL your entered is invalid");
                 } else if(content['reason'] == "captchaInvalid"){
-                    alert("Captcha result was invalid, please try again");
+                    document.getElementById("alerts").innerHTML = '<div class="alert alert-warning" role="alert">Captcha result was invalid, please try again</div>';
+                    // alert("Captcha result was invalid, please try again");
                 } else {
                     alert("Unknown error, please try again later");
                 }
             }
+            document.getElementById('spinner').innerHTML = '';
             document.getElementById("addButton").disabled = false; 
         })();
         
--- public/index.php
+++ public/index.php
@@ -1,168 +1,163 @@
 <?php
-    require __DIR__ . '/../vendor/autoload.php';
-    use Slim\Http\Response as Response;
-    use Slim\Factory\AppFactory;
-    use Slim\Views\Twig;
-    use Slim\Views\TwigMiddleware;
+require __DIR__ . '/../vendor/autoload.php';
 
-    // loading env file
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
-    $dotenv->safeLoad();
+use Slim\Http\Response as Response;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
-    $db = new MongoDB\Client($_ENV['MONGODB']);
-    $collection = $db->wakemeup->towakeup;
+// loading env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->safeLoad();
 
-    // Create App
-    $app = AppFactory::create();
+$db = new MongoDB\Client($_ENV['MONGODB']);
+$collection = $db->wakemeup->towakeup;
 
-    // Create Twig
-    if($_ENV['APP_ENV'] == "prod"){
-        $twig = Twig::create('../templates/', ['cache' => '../cache/']);
-    } else if($_ENV['APP_ENV'] == "dev"){
-        $twig = Twig::create('../templates/', ['cache' => false]); //disabling cache for debuging
-    }
-    
-    
-    // Add Twig-View Middleware
-    $app->add(TwigMiddleware::create($app, $twig));
+// Create App
+$app = AppFactory::create();
 
-    // Define named route
-    $app->get('/', function ($request, $response, $args) {
-        $view = Twig::fromRequest($request);
-        return $view->render($response, 'index.html', [
-            'hcaptchaSiteKey'=>$_ENV['hcaptchaSiteKey'],
-            'appname' => $_ENV['APP_NAME']
-        ]);
-    })->setName('index');
+// Create Twig
+if ($_ENV['APP_ENV'] == "prod") {
+    $twig = Twig::create('../templates/', ['cache' => '../cache/']);
+} else if ($_ENV['APP_ENV'] == "dev") {
+    $twig = Twig::create('../templates/', ['cache' => false]); //disabling cache for debuging
+}
 
-    // /api/v1/getURLcount GET endpoint
-    // this endpoint is used to get current documents count in db
-    $app->get('/api/v1/getURLcount', function ($request, $response, $args) {
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $twig));
+
+// Define named route
+$app->get('/', function ($request, $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.html', [
+        'hcaptchaSiteKey' => $_ENV['hcaptchaSiteKey'],
+        'appname' => $_ENV['APP_NAME']
+    ]);
+})->setName('index');
+
+// /api/v1/getURLcount GET endpoint
+// this endpoint is used to get current documents count in db
+$app->get('/api/v1/getURLcount', function ($request, $response, $args) {
+    global $collection;
+    $count = $collection->count();
+    $jsonData = array(
+        'count' => $count
+    );
+    return $response->withJson($jsonData, 200);
+})->setName('getURLcount');
+
+// /api/v1/cron GET endpoint
+// this endpoint is used to executing pinging to websites
+// it is required to provide secret that is set in .env file to execute
+$app->get('/api/v1/cron', function ($request, $response, $args) {
+    $secret = $request->getQueryParams()['secret'];
+    if ($secret == $_ENV['APP_CRONSECRET']) {
         global $collection;
-        $count = $collection->count();
-        $jsonData = array(
-            'count'=>$count
-        );
-        return $response->withJson($jsonData, 200);
-    })->setName('getURLcount');
+        $documents = $collection->find()->toArray();
+        foreach ($documents as $entry) {
+            $url = $entry['url'];
 
-    // /api/v1/cron GET endpoint
-    // this endpoint is used to executing pinging to websites
-    // it is required to provide secret that is set in .env file to execute
-    $app->get('/api/v1/cron', function ($request, $response, $args) {
-        $secret = $request->getQueryParams()['secret'];
-        if($secret == $_ENV['APP_CRONSECRET']){
-            global $collection;
-            $documents = $collection->find()->toArray();
-            foreach ($documents as $entry) {
-                $url = $entry['url'];
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 3);
+            curl_setopt($curl, CURLOPT_TIMEOUT, 3); //timeout in seconds
+            curl_setopt($curl, CURLOPT_USERAGENT, $_ENV['USERAGENT']);
+            $resp = curl_exec($curl);
+            curl_close($curl);
 
-                $curl = curl_init($url);
-                curl_setopt($curl, CURLOPT_URL, $url);
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 3); 
-                curl_setopt($curl, CURLOPT_TIMEOUT, 3); //timeout in seconds
-                curl_setopt($curl, CURLOPT_USERAGENT, $_ENV['USERAGENT']);
-                $resp = curl_exec($curl);
-                curl_close($curl);
-                
-                echo $entry['url'],"<br>";
-            }
-            return $response->write('executing...');
-        } else {
-            return $response->withStatus(403);
+            echo $entry['url'], "<br>";
         }
-        
-    })->setName('cron');
+        return $response->write('executing...');
+    } else {
+        return $response->withStatus(403);
+    }
+})->setName('cron');
 
-    // /api/v1/addURL POST endpoint
-    // this endpoint is used to add a new link to system
-    $app->post('/api/v1/addURL', function ($request, $response, $args) {
-        global $collection;
+// /api/v1/addURL POST endpoint
+// this endpoint is used to add a new link to system
+$app->post('/api/v1/addURL', function ($request, $response, $args) {
+    global $collection;
 
-        // retriving body and parsing json into array
-        $json = $request->getBody();
-        $data = json_decode($json, true);
-        
-        $url = $data['url'];
-        $hcaptchaResponse = $data['captchaResponse'];
-        
+    // retriving body and parsing json into array
+    $json = $request->getBody();
+    $data = json_decode($json, true);
 
-        // checking if array is not empty and value is a valid url
-        if(!empty($data['url']) && filter_var($url, FILTER_VALIDATE_URL)){
+    $url = $data['url'];
+    $hcaptchaResponse = $data['captchaResponse'];
 
-            $verifyingCaptcha = validateCaptcha($hcaptchaResponse);
 
-            if($verifyingCaptcha){
+    // checking if array is not empty and value is a valid url
+    if (!empty($data['url']) && filter_var($url, FILTER_VALIDATE_URL)) {
 
-                $checkIfExist = $collection->find( [ 'url' => $url] )->toArray();
-                // checking if url is not already in db
-                if(!empty($checkIfExist[0]['url'])){
-                    // if it is, returning 200 http code with success = false
-                    $data = array(
-                        'success'=> false,
-                        'reason'=>'urlExist'
-                    );
-                    return $response->withJson($data, 200);
+        $verifyingCaptcha = validateCaptcha($hcaptchaResponse);
 
-                } else { // if it not exist - inserting into db and returning result
-                    $addNewURL = $collection->insertOne(
-                        [
-                            'url'=>$url,
-                            'date'=>microtime(true)
-                        ]
-                    );
-                    // creating array with json response
-                    $data = array(
-                        'success'=> true,
-                        'id'=>$addNewURL->getInsertedId()
-                    );
+        if ($verifyingCaptcha) {
 
-                    return $response->withJson($data, 200);
-
-                }
-            } else { // if captcha validation failed
+            $checkIfExist = $collection->find(['url' => $url])->toArray();
+            // checking if url is not already in db
+            if (!empty($checkIfExist[0]['url'])) {
+                // if it is, returning 200 http code with success = false
                 $data = array(
-                    'success'=> false,
-                    'reason'=>'captchaInvalid'
+                    'success' => false,
+                    'reason' => 'urlExist'
                 );
                 return $response->withJson($data, 200);
+            } else { // if it not exist - inserting into db and returning result
+                $addNewURL = $collection->insertOne(
+                    [
+                        'url' => $url,
+                        'date' => microtime(true)
+                    ]
+                );
+                // creating array with json response
+                $data = array(
+                    'success' => true,
+                    'id' => $addNewURL->getInsertedId()
+                );
+
+                return $response->withJson($data, 200);
             }
-            
-        } else { // if body is empty
+        } else { // if captcha validation failed
             $data = array(
-                    'success'=> false,
-                    'reason'=>'missingBodyOrBadURL'
+                'success' => false,
+                'reason' => 'captchaInvalid'
             );
             return $response->withJson($data, 200);
-                
         }
-
-    })->setName('addNew');
-
-    function validateCaptcha($response){
+    } else { // if body is empty
         $data = array(
-            'secret' => $_ENV['hcaptchaSecret'],
-            'response' => $response
+            'success' => false,
+            'reason' => 'missingBodyOrBadURL'
         );
-        $verify = curl_init();
-        curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
-        curl_setopt($verify, CURLOPT_POST, true);
-        curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($verify);
-        // var_dump($response);
-        $responseData = json_decode($response);
-        if($responseData->success) {
-            // your success code goes here
-            return true;
-        } 
-        else {
-            // return error to user; they did not pass
-            return false;
-        }
+        return $response->withJson($data, 200);
     }
+})->setName('addNew');
+
+function validateCaptcha($response)
+{
+    $data = array(
+        'secret' => $_ENV['hcaptchaSecret'],
+        'response' => $response
+    );
+    $verify = curl_init();
+    curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
+    curl_setopt($verify, CURLOPT_POST, true);
+    curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($verify);
+    // var_dump($response);
+    $responseData = json_decode($response);
+    if ($responseData->success) {
+        // your success code goes here
+        return true;
+    } else {
+        // return error to user; they did not pass
+        return false;
+    }
+}
 
 
-    // Run app
-    $app->run();
+// Run app
+$app->run();
